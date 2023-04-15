b5b0a0f1c174648e54004a33368c43e22988fc4c
Test <hepsie50@gmail.com>
2023-04-15 20:57:54 +0900

    update

---
+++ install.sh
@@ -0,0 +1,7 @@
+pip install mpi4py==3.1.4
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install numpy pandas matplotlib tqdm scikit-learn pybind11 pybind11[global] ipykernel notebook
+pip install pytest
+pip install flwr
+pip install grad-cam
+pip install timm
